5db3a05bb67f46e13fae9f541081f8a6f2c419ec
Roy <jackman.roy@gmail.com>
2020-09-06 22:57:50 -0400

    Generalization and command line interface

---
+++ Processer.py
@@ -0,0 +1,73 @@
+from openpyxl import load_workbook
+from optparse import OptionParser
+import os
+import pandas as pd
+import YNetScraper as yns
+
+
+def url_sorter(name, writer, url, start_year, end_year):
+    if yns.BASE_URL in url:
+        yns.url_sorter(name, writer, url, start_year, end_year)
+
+
+parser = OptionParser()
+parser.add_option('-b',
+                  '--book',
+                  dest='book',
+                  type='string',
+                  default=None,
+                  help='Name of the Excel book to write to')
+parser.add_option('-s',
+                  '--start_year',
+                  dest='start_year',
+                  type='int',
+                  default=2015,
+                  help='Year to start parsing')
+parser.add_option('-e',
+                  '--end_year',
+                  dest='end_year',
+                  type='int',
+                  default=2020,
+                  help='Year to end parsing (inclusive)')
+parser.add_option('-n',
+                  '--sheet_name',
+                  action='append',
+                  dest='sheet_names',
+                  default=[],
+                  help='Names of sheets in order of url')
+parser.add_option('-u',
+                  '--url',
+                  action='append',
+                  dest='urls',
+                  help='Urls to parse')
+
+(options, args) = parser.parse_args()
+if options.book != None and ('.xlsx' not in options.book[-5:]):
+    options.book += '.xlsx'
+if options.book == None:
+    options.book = 'output.xlsx'
+default = 0
+book = None
+
+try:
+    book = load_workbook(filename=options.book)
+except:
+    print('Could not find existing book, creating one at', options.book)
+
+with pd.ExcelWriter(options.book, engine='openpyxl', mode='a') as writer:
+    if book is not None:
+        writer.book = book
+    for i in range(len(options.urls)):
+        if i < len(options.sheet_names):
+            name = options.sheet_names[i]
+        else:
+            name = f'default_name_{default}'
+            default += 1
+
+        url_sorter(
+            name,
+            writer,
+            options.urls[i],
+            options.start_year,
+            options.end_year
+        )
--- YNetScraper.py
+++ YNetScraper.py
@@ -11,19 +11,27 @@ multitasking.set_max_threads(10)
 
 BASE_URL = 'https://www.ynet.co.il'
 
+
+def url_sorter(name, writer, url, start_year, end_year):
+    if '/home/0,7340,L' in url:
+        match = re.search(r'4269-[0-9]{3}[0-9]?-[0-9]+', url)
+        lcode = match.group(0)
+        parse_range(start_year, end_year + 1, lcode, writer, name)
+
+
 def parse_article(url, month, year):
     articleUrl = BASE_URL + url
     articlehandle = urllib.request.urlopen(articleUrl)
     while articlehandle.getcode() != 200:
         articlehandle = urllib.request.urlopen(articleUrl)
     soup = bs(articlehandle.read(), 'html.parser')
-    falist = soup.find_all('span',class_='art_header_footer_author')
+    falist = soup.find_all('span', class_='art_header_footer_author')
     try:
-        title = soup.find('h1',class_='art_header_title').contents[0]
+        title = soup.find('h1', class_='art_header_title').contents[0]
     except:
         title = None
 
-    try: 
+    try:
         author = falist[0].contents[0].contents[0]
     except:
         author = None
@@ -42,8 +50,9 @@ def parse_article(url, month, year):
     ]
     return retval
 
+
 def parse_month(year, month, page, lcode):
-    url = f'https://www.ynet.co.il/home/0,7340,L-{lcode}-20{year}{str(month).zfill(2)}-{page},00.html'
+    url = f'https://www.ynet.co.il/home/0,7340,L-{lcode}-{year}{str(month).zfill(2)}-{page},00.html'
     filehandle = urllib.request.urlopen(url)
     error = False
     while error or filehandle.getcode() != 200:
@@ -53,14 +62,15 @@ def parse_month(year, month, page, lcode):
         except:
             error = True
     soup = bs(filehandle.read(), 'html.parser')
-    table = soup.find('td', class_='ghciArticleIndex1').contents[2]
+    table = soup.find('td', class_='ghciArticleIndex1')
+    table = table.contents[2]
     articles = len(table.contents)
     retval = []
     if articles > 1:
         for i in range(int(articles/3)):
             link = table.contents[3 * i].find('a')
             metadata = table.contents[3 * i + 1].string.split(' ')
-            dates = metadata[-1].replace('(', '').replace(')','').split('/')
+            dates = metadata[-1].replace('(', '').replace(')', '').split('/')
             retval.append([
                 link.string,
                 ' '.join(metadata[:-1]),
@@ -73,11 +83,11 @@ def parse_month(year, month, page, lcode):
         retval += parse_month(year, month, page + 1, lcode)
     return retval
 
-# @multitasking.task
+
 def parse_range(start_year, end_year, lcode, writer, name):
-    columns=[
+    columns = [
         'כותרת הידיעה',
-        'שם הכותב', 
+        'שם הכותב',
         'תאריך',
         'חודש',
         'שנה',
@@ -89,43 +99,7 @@ def parse_range(start_year, end_year, lcode, writer, name):
             month = parse_month(i, j, 1, lcode)
             if len(month) > 0:
                 df += month
-            print(name[::-1], 'Month ', j, '/', '20' + str(i), 'finished at', time.time() - start_time)
+            print(name, 'Month ', j, '/', i)
     DFrame = pd.DataFrame(df, columns=columns)
     print(DFrame)
     DFrame.to_excel(writer, sheet_name=name, index=False, verbose=True)
-    # DFrame.to_csv(path_or_buf=writer, index=False)
-
-start_time = time.time()
-first_time = start_time
-
-@multitasking.task
-def politics():
-    with pd.ExcelWriter('politics.xlsx') as writer:
-        parse_range(15, 21, '4269-890-315', writer, 'מדיני')
-        parse_range(15, 21, '4269-891-317', writer, 'המערכת הפוליטית')
-        parse_range(15, 21, '4269-109-185', writer, 'פוליטי מדיני')
-
-@multitasking.task
-def army():
-    with pd.ExcelWriter('army.xlsx') as writer:
-        parse_range(15, 21, '4269-141-344', writer, 'צבא וביטחון')
-        parse_range(15, 21, '4269-3259-4172', writer, 'פלסטינים')
-        parse_range(15, 21, '4269-3602-4502', writer, 'רון בן ישי')
-
-@multitasking.task
-def economics():
-    with pd.ExcelWriter('economics.xlsx') as writer:
-        parse_range(15, 21, '4269-116-429', writer, 'כלכלה בארץ')
-        parse_range(15, 21, '4269-117-430', writer, 'כלכלה בעולם')
-        parse_range(15, 21, '4269-118-431', writer, 'הייטק')
-        parse_range(15, 21, '4269-271-750', writer, 'מיוחד')
-        parse_range(15, 21, '4269-583-1336', writer, 'בורסה')
-        parse_range(15, 21, '4269-3757-5363', writer, 'צרכנות')
-        parse_range(15, 21, '4269-3751-6567', writer, 'מטבח חוץ')
-        parse_range(15, 21, '4269-3747-7615', writer, 'הכסף שלי')
-        parse_range(15, 21, '4269-3749-8091', writer, 'קריירה')
-        parse_range(15, 21, '4269-3755-8315', writer, 'נדלן')
-
-politics()
-army()
-economics()
