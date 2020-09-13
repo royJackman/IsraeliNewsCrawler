2f8ee3284cc07cbadc2d6c8746bfe0a5f107d2ea
royJackman <jackman.roy@gmail.com>
2020-09-13 18:25:31 -0400

    options cleanup

--- Processer.py
+++ Processer.py
@@ -13,35 +13,11 @@ def url_sorter(name, writer, url, start_year, end_year):
 
 
 parser = OptionParser()
-parser.add_option('-b',
-                  '--book',
-                  dest='book',
-                  type='string',
-                  default=None,
-                  help='Name of the Excel book to write to')
-parser.add_option('-s',
-                  '--start_year',
-                  dest='start_year',
-                  type='int',
-                  default=2015,
-                  help='Year to start parsing')
-parser.add_option('-e',
-                  '--end_year',
-                  dest='end_year',
-                  type='int',
-                  default=2020,
-                  help='Year to end parsing (inclusive)')
-parser.add_option('-n',
-                  '--sheet_name',
-                  action='append',
-                  dest='sheet_names',
-                  default=[],
-                  help='Names of sheets in order of url')
-parser.add_option('-u',
-                  '--url',
-                  action='append',
-                  dest='urls',
-                  help='Urls to parse')
+parser.add_option('-b', '--book', dest='book', type='string', default=None, help='Name of the Excel book to write to')
+parser.add_option('-s', '--start_year', dest='start_year', type='int', default=2015, help='Year to start parsing')
+parser.add_option('-e', '--end_year', dest='end_year', type='int', default=2020, help='Year to end parsing (inclusive)')
+parser.add_option('-n', '--sheet_name', action='append', dest='sheet_names', default=[], help='Names of sheets in order of url')
+parser.add_option('-u', '--url', action='append', dest='urls', help='Urls to parse')
 
 (options, args) = parser.parse_args()
 if options.book != None and ('.xlsx' not in options.book[-5:]):
